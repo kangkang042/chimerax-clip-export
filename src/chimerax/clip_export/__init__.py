945a2a3424d05f901d86e60be761ad62a9fbec50
kangkang042 <kangkang042@163.com>
2026-05-18 18:04:21 +0800

    feat: scaffold chimerax-clip-export bundle project

---
+++ src/chimerax/clip_export/__init__.py
@@ -0,0 +1,10 @@
+# src/chimerax/clip_export/__init__.py
+# Bundle registration for chimerax-clip-export
+from chimerax.core.toolshed import BundleInfo
+
+
+class _BundleInfo(BundleInfo):
+    pass
+
+
+bundle_info = _BundleInfo()
